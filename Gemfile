5dedc34cb96836a15100e2f2108073f486eb7a3a
Karthiga <karthigaanand14@gmail.com>
2022-05-02 12:01:43 +0530

    visitor_scaffold

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "sassc-rails"
 
 gem 'bootstrap', '~> 5.1', '>= 5.1.3'
 
+gem 'devise', '~> 4.8', '>= 4.8.1'
+
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_084502) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_060635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_28_084502) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "visitor_tables", force: :cascade do |t|
+    t.string "name"
+    t.string "relation"
+    t.string "classification"
+    t.integer "no_of_visits"
+    t.datetime "last_visited"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
